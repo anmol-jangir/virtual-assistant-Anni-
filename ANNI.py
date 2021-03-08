50c8d3ad085099264ed052f049dfab3d9895c455
Anmol Jangir <62926653+anmol-jangir@users.noreply.github.com>
2021-03-08 15:21:38 +0530

    Update ANNI.py

--- ANNI.py
+++ ANNI.py
@@ -1,10 +1,8 @@
 import subprocess
 import wolframalpha
 import pyttsx3
-import tkinter
 import json
 import random
-import operator
 import speech_recognition as sr
 import datetime
 import wikipedia
@@ -13,7 +11,6 @@ import os
 import time
 import winshell
 import pyjokes
-import feedparser
 import smtplib
 import ctypes
 import time
@@ -22,8 +19,6 @@ import shutil
 from twilio.rest import Client
 from clint.textui import progress
 from ecapture import ecapture as ec
-from bs4 import BeautifulSoup
-import win32com.client as wincl
 from urllib.request import urlopen
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -90,15 +85,14 @@ def sendEmail(to, content):
 	server.starttls()
 	
 	# Enable low security in gmail
-	server.login('anmoooljangir@gmail.com', '24november2019')
-	server.sendmail('anmoooljangir@gmail.com', to, content)
+	server.login('enter your email', '*******')
+	server.sendmail('enter your email', to, content)
 	server.close()
 
 if __name__ == '__main__':
 	clear = lambda: os.system('cls')
 	
-	# This Function will clean any
-	# command before execution of this python file
+	# This Function will clean any command before execution of this python file
 	clear()
 	wishMe()
 	usrname()
@@ -107,10 +101,7 @@ if __name__ == '__main__':
 		
 		query = takeCommand().lower()
 		
-		# All the commands said by user will be 
-		# stored here in 'query' and will be
-		# converted to lower case for easily 
-		# recognition of command
+		# All the commands said by user will be stored here in 'query' and will be converted to lower case for easily recognition of command
 		if 'wikipedia' in query:
 			speak('Searching Wikipedia...')
 			query = query.replace("wikipedia", "")
@@ -118,19 +109,22 @@ if __name__ == '__main__':
 			speak("According to Wikipedia")
 			print(results)
 			speak(results)
-
+                 #Youtube is open by this condition
 		elif 'open youtube' in query:
 			speak("Here you go to Youtube\n")
 			webbrowser.open("youtube.com")
-
+			
+                 #Open google website by this condition in default broswer 
 		elif 'open google' in query:
 			speak("Here you go to Google\n")
 			webbrowser.open("google.com")
-
+			
+                  #Open stackoverflow website by this condition in default broswer
 		elif 'open stackoverflow' in query:
 			speak("Here you go to Stack Over flow.Happy coding")
 			webbrowser.open("stackoverflow.com") 
-
+                  
+		#Play music by default which play list you have in your pc   
 		elif 'play music' in query or "play song" in query:
 			speak("Here you go with music")
 			# music_dir = "G:\\Song"
@@ -138,24 +132,25 @@ if __name__ == '__main__':
 			songs = os.listdir(music_dir)
 			print(songs) 
 			random = os.startfile(os.path.join(music_dir, songs[1]))
-
+                
+		#it shows you time 
 		elif 'the time' in query:
 			strTime =time.strftime("%m-%d-%Y %T:%M%p") 
 			speak(f"Sir, the time is {strTime}")
 
 		
-
+               #this will help you to create  mail 
 		elif 'mail  ' in query:
 			try:
 				speak("What should I say?")
 				content = takeCommand()
-				to = "dubey3111995@gmail.com"
+				to = "sending email "
 				sendEmail(to, content)
 				speak("Email has been sent !")
 			except Exception as e:
 				print(e)
 				speak("I am not able to send this email")
-
+                #this will help you to send mail 
 		elif 'send a mail' in query:
 			try:
 				speak("What should I say?")
@@ -167,41 +162,50 @@ if __name__ == '__main__':
 			except Exception as e:
 				print(e)
 				speak("I am not able to send this email")
-
+                  
+		#this condition response you like a human talk 
 		elif 'how are you' in query:
 			speak("I am fine, Thank you")
 			speak("How are you, Sir")
-
+                
+		#this condition response you like a human talk 
 		elif 'fine' in query or "good" in query:
 			speak("It's good to know that your fine")
-
+                 
+		#this condition will help you to change user name 
 		elif "change my name to" in query:
 			query = query.replace("change my name to", "")
 			assname = query
-
+               
+	         #this condition will hepl you to change name of your virtual assistant 
 		elif "change name" in query:
 			speak("What would you like to call me, Sir ")
 			assname = takeCommand()
 			speak("Thanks for naming me")
-
+                
+		#this condition will return you very friendly answers
 		elif "what's your name" in query or "What is your name" in query:
 			speak("My friends call me")
 			speak(assname)
 			print("My friends call me", assname)
-
+                 
+		#this will finish the commands of your program and finish the program 
 		elif 'exit' in query:
 			speak("Thanks for giving me your time")
 			exit()
-
+                    
+		#this condition give you information about developer name 
 		elif "who made you" in query or "who created you" in query: 
-			speak("I have been created by Gaurav.")
-			
+			speak("I have been created by Anmol .")
+		
+		#through this condition anni crack a joke for you 
 		elif 'joke' in query:
 			speak(pyjokes.get_joke())
-			
+		
+		#this condtion will return you any query answer using AI algo's 
 		elif "calculate" in query: 
 			
-			app_id = 'HKH5G9-YWW72E5XE6'
+			app_id = 'walform api '
 			client = wolframalpha.Client(app_id)
 			indx = query.lower().split().index('calculate') 
 			query = query.split()[indx + 1:] 
@@ -209,30 +213,36 @@ if __name__ == '__main__':
 			answer = next(res.results).text
 			print("The answer is " + answer) 
 			speak("The answer is " + answer) 
-
+                
+		#this condtion will  help to search
 		elif 'search' in query or 'play' in query:
 			
 			query = query.replace("search", "") 
 			query = query.replace("play", "")		 
 			webbrowser.open(query) 
-
+ 
+                #this condtion will friendly reslut like human 
 		elif "who i am" in query:
 			speak("If you talk then definately your human.")
-
+ 
+                #this condition will result of creating anni
 		elif "why you came to world" in query:
 			speak("Thanks to Anmol. further It's a secret")
 
 		
-
-		elif 'is love' in query:
-			speak("It is 7th sense that destroy all other senses")
-
-		elif "who are you" in query:
+                #this condtion will friendly result like human 
+                elif 'is love' in query:
+                       speak("It is 7th sense that destroy all other senses")
+ 
+                #this condtion will friendly result like human 
+                 elif "who are you" in query:
 			speak("I am your virtual assistant created by Anmol ")
-
-		elif 'reason for you' in query:
+                
+		#this condtion will friendly result like human 
+                 elif 'reason for you' in query:
 			speak("I was created as a Minor project by Anmol ")
 
+		#this condition will change background of your pc 	
 		elif 'change background' in query:
 			ctypes.windll.user32.SystemParametersInfoW(20, 
 													0, 
@@ -241,7 +251,7 @@ if __name__ == '__main__':
 			speak("Background changed succesfully")
 
 		
-
+                #this condition will return you news 
 		elif 'news' in query:
 			
 			try: 
@@ -263,46 +273,56 @@ if __name__ == '__main__':
 				print(str(e))
 
 		
+		#this condtion willlock window of your pc 
 		elif 'lock window' in query:
 				speak("locking the device")
 				ctypes.windll.user32.LockWorkStation()
-
+                 
+		#this condtion will shutdown system your pc 
 		elif 'shutdown system' in query:
 				speak("Hold On a Sec ! Your system is on its way to shut down")
 				subprocess.call('shutdown / p /f')
-				
+		
+		#this condtion will empty recycle bin your pc 
 		elif 'empty recycle bin' in query:
 			winshell.recycle_bin().empty(confirm = False, show_progress = False, sound = True)
 			speak("Recycle Bin Recycled")
-
+			
+                #this condtion will stop music  your pc 
 		elif "don't listen" in query or "stop listening" in query:
 			speak("for how much time you want to stop anni from listening commands")
 			a = int(takeCommand())
 			time.sleep(a)
 			print(a)
 
+                 #this condition will gives you location of any where  	
 		elif "where is" in query:
 			query = query.replace("where is", "")
 			location = query
 			speak("User asked to Locate")
 			speak(location)
 			webbrowser.open("https://www.google.nl / maps / place/" + location + "")
-
+           
+	        #this condtion will turn on camera your pc 
 		elif "camera" in query or "take a photo" in query:
 			ec.capture(0, "Jarvis Camera ", "img.jpg")
 
+		#this condtion will restart your pc 	
 		elif "restart" in query:
 			subprocess.call(["shutdown", "/r"])
-			
+		
+		#this condtion will hibernate your pc 
 		elif "hibernate" in query or "sleep" in query:
 			speak("Hibernating")
 			subprocess.call("shutdown / h")
 
+		#this condtion will log off your pc 	
 		elif "log off" in query or "sign out" in query:
 			speak("Make sure all the application are closed before sign-out")
 			time.sleep(5)
 			subprocess.call(["shutdown", "/l"])
 
+		#this conition will help to create a document 	
 		elif "write a note" in query:
 			speak("What should i write, sir")
 			note = takeCommand()
@@ -317,6 +337,7 @@ if __name__ == '__main__':
 			else:
 				file.write(note)
 		
+		#this condition will gives show you notes that you create 
 		elif "show note" in query:
 			speak("Showing Notes")
 			file = open("new.txt", "r") 
@@ -324,18 +345,16 @@ if __name__ == '__main__':
 			speak(file.read(6))
 
 		
+		#this condition will gives you  result that anni is there for you 
 		elif "Anni" in query:
 			
 			wishMe()
 			speak("anni in your service Mister")
 			speak(assname)
 
+		#this condition gives weather report of any location 	
 		elif "weather" in query:
-			
-			# Google Open weather website
-			# to get API of Open weather 
-			#api_key = "a6cdf9407aa5de036875b3fdf55f5738"
-			base_url = "http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q="
+			base_url = "http://api.openweathermap.org/data/2.5/weather?appid=api_id&q="
 			speak(" City name ")
 			print("City name : ")
 			city_name = takeCommand()
@@ -356,21 +375,26 @@ if __name__ == '__main__':
 				speak(" City Not Found ")
 			
 		
-
+ 
+                #this condition is give result of query from wikipedia 
 		elif "wikipedia" in query:
 			webbrowser.open("wikipedia.com")
 
+		#this condition wishes you when you first intract with anni 	
 		elif "Good Morning" in query:
 			speak("A warm" +query)
 			speak("How are you Mister")
 			speak(assname)
 
+		#this will give you very friendly result	
 		elif "will you be my gf" in query or "will you be my bf" in query: 
 			speak("I'm not sure about, may be you should give me some time")
 
+		#this will give you very friendly result	
 		elif "how are you" in query:
 			speak("I'm fine, glad you me that")
-
+ 
+                  #this will give you very friendly result
 		elif "i love you" in query:
 			speak("It's hard to understand")
 
